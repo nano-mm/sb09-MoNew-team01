cc874514a1e3ef59d18b766dc8fa5ef5ce3b8dad
Alpha <mathematic026@gmail.com>
2026-04-14 18:57:57 +0900

    feat: UserDto

---
+++ src/main/java/com/monew/dto/response/UserDto.java
@@ -0,0 +1,14 @@
+package com.monew.dto.response;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record UserDto(
+    UUID id,
+    String email,
+    String nickname,
+    Instant createdAt
+) {
+
+}
