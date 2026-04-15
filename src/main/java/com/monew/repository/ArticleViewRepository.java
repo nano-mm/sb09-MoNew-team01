b6cd1b02e029f19479d6f2f3fb7b41b597cce114
한성재 <anis15740@gmail.com>
2026-04-15 11:25:29 +0900

    feat: ArticleController 추가

--- src/main/java/com/monew/repository/ArticleViewRepository.java
+++ src/main/java/com/monew/repository/ArticleViewRepository.java
@@ -1,9 +1,14 @@
 package com.monew.repository;
 
+import com.monew.entity.Article;
 import com.monew.entity.ArticleView;
+import com.monew.entity.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleViewRepository extends JpaRepository<ArticleView, UUID> {
 
+  ArticleView findByIdAndArticle(UUID id, Article article);
+
+  ArticleView findByArticleAndUser(Article article, User user);
 }
