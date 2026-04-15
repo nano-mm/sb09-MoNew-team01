80a56dde090a571ce941809ce82eb7ab2213e667
Alpha <mathematic026@gmail.com>
2026-04-15 10:00:09 +0900

    feat: User Login/Update용 메서드 추가

--- src/main/java/com/monew/repository/UserRepository.java
+++ src/main/java/com/monew/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.monew.repository;
 
 import com.monew.entity.User;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+  Optional<User> findByEmail(@Param("email") String email);
+
+  Optional<User> findById(@Param("id") UUID id);
 }
