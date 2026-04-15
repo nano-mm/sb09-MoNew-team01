74b2f406d1c4ebda5d0ea5e2ff005f8b7a216989
Alpha <mathematic026@gmail.com>
2026-04-15 10:24:31 +0900

    fix: @Email Validation에서 @NotBlank, @Length 제약 조건 추가
    ref: https://github.com/nano-mm/sb09-MoNew-team01/pull/1#discussion_r3083216842

--- src/main/java/com/monew/dto/request/UserLoginRequest.java
+++ src/main/java/com/monew/dto/request/UserLoginRequest.java
@@ -1,10 +1,11 @@
 package com.monew.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record UserLoginRequest(
-    @Email String email,
+    @NotBlank @Email @Length ( min = 1 , max = 254 )  String email,
     @Length(min = 6, max = 20) String password
 ) {
 
--- src/main/java/com/monew/dto/request/UserRegisterRequest.java
+++ src/main/java/com/monew/dto/request/UserRegisterRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record UserRegisterRequest(
-    @Email String email,
+    @NotBlank @Email @Length ( min = 1 , max = 254 )  String email,
     @NotBlank @Length(min = 1, max = 20) String nickname,
     @NotBlank @Length(min = 6, max = 20) String password
 ) {
