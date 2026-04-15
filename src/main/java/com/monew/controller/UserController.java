440bf9b3c6a6748320230951fe3e2b6bb2d127fd
Alpha <mathematic026@gmail.com>
2026-04-15 10:00:55 +0900

    feat: User Login/Update EndPoint 추가

--- src/main/java/com/monew/controller/UserController.java
+++ src/main/java/com/monew/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.monew.controller;
 
+import com.monew.dto.request.UserLoginRequest;
 import com.monew.dto.request.UserRegisterRequest;
+import com.monew.dto.request.UserUpdateRequest;
 import com.monew.dto.response.UserDto;
 import com.monew.service.UserService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +25,15 @@ public class UserController {
   @PostMapping
   public ResponseEntity<UserDto> create(@RequestBody @Valid UserRegisterRequest request){
     return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
+  }
+
+  @PostMapping("login")
+  public ResponseEntity<UserDto> login(@RequestBody @Valid UserLoginRequest request){
+    return ResponseEntity.ok().body(userService.login(request));
+  }
+
+  @PatchMapping("{userId}")
+  public ResponseEntity<UserDto> update(@PathVariable UUID userId, @RequestBody @Valid UserUpdateRequest request) {
+    return ResponseEntity.ok().body(userService.update(userId, request));
   }
 }
