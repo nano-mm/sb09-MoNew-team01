d64d65d544f37b40ea692c5156d30d186984937a
Alpha <mathematic026@gmail.com>
2026-04-15 09:51:00 +0900

    fix: autoincrease 삭제

--- src/main/java/com/monew/entity/base/BaseEntity.java
+++ src/main/java/com/monew/entity/base/BaseEntity.java
@@ -19,7 +19,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   protected UUID id;
 
   @JsonIgnore
