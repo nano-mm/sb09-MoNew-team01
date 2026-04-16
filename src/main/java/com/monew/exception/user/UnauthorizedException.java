ee407852665bd5ac5b3f8f92eeadd47361deef14
Alpha <mathematic026@gmail.com>
2026-04-16 11:44:32 +0900

    Fix: UnauthorizedException 상태코드 제대로 출력되게끔 수정
    ref: https://github.com/nano-mm/sb09-MoNew-team01/pull/10#discussion_r3090350038

--- src/main/java/com/monew/exception/user/UnauthorizedException.java
+++ src/main/java/com/monew/exception/user/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package com.monew.exception.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
 
   public UnauthorizedException(String message) {
