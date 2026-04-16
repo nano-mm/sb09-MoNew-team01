290f63949f0e8ed159f03f4dfe1db9002bc42568
hyemin <hyemin.ellen.97@gmail.com>
2026-04-16 14:19:05 +0900

    feat:interest수정,삭제 구현

---
+++ build/generated/sources/annotationProcessor/java/main/com/monew/mapper/UserMapperImpl.java
@@ -0,0 +1,38 @@
+package com.monew.mapper;
+
+import com.monew.dto.response.UserDto;
+import com.monew.entity.User;
+import java.time.Instant;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-04-15T17:41:13+0900",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.4.jar, environment: Java 17.0.18 (Amazon.com Inc.)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto toDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UUID id = null;
+        String email = null;
+        String nickname = null;
+        Instant createdAt = null;
+
+        id = user.getId();
+        email = user.getEmail();
+        nickname = user.getNickname();
+        createdAt = user.getCreatedAt();
+
+        UserDto userDto = new UserDto( id, email, nickname, createdAt );
+
+        return userDto;
+    }
+}
